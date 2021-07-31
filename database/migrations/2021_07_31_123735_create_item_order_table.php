4b6662ecf7d6f46dcc98a2676dff06a8fd5b62b9
Nael Mansour <naelnael53.nm@gmail.com>
2021-07-31 15:15:40 +0200

    orders-migrations

---
+++ database/migrations/2021_07_31_123735_create_item_order_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::enableForeignKeyConstraints();
+        
+        Schema::create('item_order', function (Blueprint $table) {
+            $table->bigInteger('cart_id')->unsigned();
+            $table->bigInteger('item_id')->unsigned();
+
+            $table->foreign('cart_id')
+                ->references('id')
+                ->on('carts');
+
+            $table->foreign('item_id')
+                ->references('id')
+                ->on('items')
+                ->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_order');
+    }
+}
