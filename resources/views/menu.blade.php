029f8e8cac2b83f9e45217a74ba5c9099454cb5e
Mohamed Esmail <moh.ahmed.esmail.1403@gmail.com>
2021-08-13 03:53:27 +0200

    prepared invoice layout in menu view

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -20,27 +20,16 @@
     </div>
     -->
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="w-4xl container mx-auto sm:px-6 lg:px-8 flex flex-wrap">
 
-
-            <!-- Invoice Goes here -->
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg m-3" style="float: right">
-                <div class="p-6 bg-white border-b border-gray-200">
-
-                    <p>Invoice here</p>
-                </div>
-            </div>
-
-
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <!-- Menu Items Go Here -->
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-2/3">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-col">
                         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-
                                     <table class="min-w-full divide-y divide-gray-200">
-
                                         <thead class="bg-gray-50">
                                             <tr>
                                                 <th scope="col"
@@ -76,18 +65,82 @@
                                                     </td>
                                                     <td
                                                         class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+
+                                                        <a href="#" class="text-yellow-600 hover:text-yellow-900">
+                                                            <i style="font-size:24px" class="fa">&#xf056;</i>
+                                                        </a>
+
                                                         <a href="{{ route('FoodAddToInvoice', ['id' => $foodItems[$i]->id]) }}"
-                                                            class="text-indigo-600 hover:text-indigo-900">Add</a>
+                                                            class="text-yellow-600 hover:text-yellow-900">
+                                                            <i style="font-size:24px" class="fa">&#xf055;</i>
+                                                        </a>
                                                     </td>
                                                 </tr>
                                             @endfor
                                         </tbody>
-
                                     </table>
-
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <!-- Invoice Goes here -->
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg m-3 h-1/2 w-1/4">
+                <div class="p-6 bg-white border-b border-gray-200">
 
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div>
+                                    <h2 class="font-semibold text-lg mb-8">Your Order</h2>
+                                    <table class="min-w-full divide-y">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col"
+                                                    class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Quantity
+                                                </th>
+                                                <th
+                                                    class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Item
+                                                </th>
+                                                <th
+                                                    class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    Sub-total
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-wrap text-sm text-gray-500">
+
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-wrap text-sm text-gray-500">
+
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-wrap text-sm text-gray-500">
+
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <hr>
+                                    <br>
+                                    <p>Taxes</p>
+                                    <p style="text-align: right">???</p>
+                                    <br>
+                                    <p>Total</p>
+                                    <p style="text-align: right">???</p>
+                                    <br>
+                                    <button class="float-right">
+                                        <a href="#"
+                                            class="inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                            Place Order
+                                        </a>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -95,5 +148,4 @@
             </div>
         </div>
     </div>
-
 </x-app-layout>
